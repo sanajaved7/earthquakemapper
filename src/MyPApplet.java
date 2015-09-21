5e8d117176e37f707d297e8eaa5097e5e09b0a3a
Sana Javed <sanajaved7@gmail.com>
2015-09-21 18:45:52 -0400

    Added shapes to GUI

--- src/MyPApplet.java
+++ src/MyPApplet.java
@@ -10,11 +10,13 @@ public class MyPApplet extends PApplet
 	{
 		size(200, 200);
 		backgroundImg = loadImage(URL, "jpg");
-	}
+	} 
 	
 	public void draw()
 	{
 		backgroundImg.resize(0, height);
 		image(backgroundImg, 0, 0);
+		fill(255,209, 0);
+		ellipse(width/4, height/5, width/5, height/5 );
 	}
 }
