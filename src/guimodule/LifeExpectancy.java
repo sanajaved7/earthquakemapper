74f4573aaa66e6814a48e931029e85d347a784af
Sana Javed <sanajaved7@gmail.com>
2015-09-26 18:20:52 -0400

    Displayed life expectancy data for each country

--- src/guimodule/LifeExpectancy.java
+++ src/guimodule/LifeExpectancy.java
@@ -1,16 +1,24 @@
 package guimodule;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import de.fhpotsdam.unfolding.UnfoldingMap;
-import de.fhpotsdam.unfolding.providers.Google;
-import de.fhpotsdam.unfolding.utils.MapUtils;
 import processing.core.PApplet;
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import de.fhpotsdam.unfolding.providers.*;
+import de.fhpotsdam.unfolding.providers.Google.*;
+
+import java.util.List;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+import de.fhpotsdam.unfolding.marker.Marker;
+
+import java.util.HashMap;
 
 public class LifeExpectancy extends PApplet {
+	
 	UnfoldingMap map;
 	HashMap<String, Float> lifeExpMap;
+	List<Feature> countries;
+	List<Marker> countryMarkers;
 	
 	public void setup() {
 		size(800, 600, OPENGL);
@@ -18,8 +26,16 @@ public class LifeExpectancy extends PApplet {
 		MapUtils.createDefaultEventDispatcher(this, map);
 		
 		//loads life expectancy data
-		lifeExpMap = (HashMap<String, Float>) loadLifeExpectancyFromCSV("../../data/LifeExpectancyWorldBank.csv");
+		lifeExpMap = loadLifeExpectancyFromCSV("../../data/LifeExpectancyWorldBankModule3.csv");
 		println("Loaded " + lifeExpMap.size() + " data entries");
+		
+		//creates 1 Feature and 1 Marker per country
+		countries = GeoJSONReader.loadData(this, "../../data/countries.geo.json");
+		countryMarkers = MapUtils.createSimpleMarkers(countries);
+		map.addMarkers(countryMarkers);
+				
+		//countries are shaded according to life expectancy
+		shadeCountries();
 	}
 	
 	public void draw() {
@@ -27,22 +43,34 @@ public class LifeExpectancy extends PApplet {
 		map.draw();
 	}
 	
+	private void shadeCountries() {
+		for (Marker marker: countryMarkers) {
+			String countryId = marker.getId();
+			
+			if (lifeExpMap.containsKey(countryId)) {
+				float lifeExp = lifeExpMap.get(countryId);
+				int colorLevel = (int) map(lifeExp, 40, 90, 10, 255);
+				marker.setColor(color(255-colorLevel, 100, colorLevel));
+			}
+			else {
+				marker.setColor(color(150, 150, 150));
+			}
+		}
+	}
+	
 	//helper method to load life expectancy data from CSV
-	private Map<String, Float> loadLifeExpectancyFromCSV(String fileName) { 
-		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
+	private HashMap<String, Float> loadLifeExpectancyFromCSV(String fileName) { 
+		HashMap<String, Float> lifeExpMap = new HashMap<String, Float>();
 		
 		String[] rows = loadStrings(fileName);
 		
 		for (String row : rows){
 			String [] columns = row.split(",");
-			if (columns[5].length() > 5) {
-				float value = Float.parseFloat(columns[5]);
-				lifeExpMap.put(columns[4], value);
+			if (columns.length == 6 && !columns[5].equals("..")) {
+				lifeExpMap.put(columns[4], Float.parseFloat(columns[5]));
 			}
 		}
-		
 		return lifeExpMap;
-		
 	}
 	
 }
